1b0ec9b4e2e531989e37a089f1ade6fa0815faf5
AllanSam <allansam0905@gmail.com>
2022-12-13 19:25:14 +0530

    fix - spellings

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
-const intialPrice = document.querySelector("#intial-price");
+const initialPrice = document.querySelector("#initial-price");
 const stocksQuantity = document.querySelector("#stocks-quantity");
 const currentPrice = document.querySelector("#Current-price");
 const stockBtn = document.querySelector("#stock-btn")
 const outputBox = document.querySelector("#output-box")
 
 
-//console.log(intialPrice, stocksQuantity, currentPrice);
+//console.log(initialPrice, stocksQuantity, currentPrice);
 //console.log(stockBtn, outputBox);
 
 function clickHandler() {
-    var intiPrice = Number(intialPrice.value);
+    var initiPrice = Number(initialPrice.value);
     var stcksQuantity = Number(stocksQuantity.value);
     var currPrice = Number(currentPrice.value);
 
-    calculateProfitAndLoss(intiPrice, stcksQuantity, currPrice);
+    calculateProfitAndLoss(initiPrice, stcksQuantity, currPrice);
 
-    intialPrice.value = "";
+    initialPrice.value = "";
     stocksQuantity.value = "";
     currentPrice.value = "";
 
 }
 
-function calculateProfitAndLoss(intial, quantity, current) {
+function calculateProfitAndLoss(initial, quantity, current) {
 
-    if (intial < 0 || quantity < 0 || current < 0) {
+    if (initial < 0 || quantity < 0 || current < 0) {
         showMessage("Enter valid values")
         return;
     }
 
-    if (intial > current) { // loss logic here 
-        var loss = (intial - current) * quantity;
-        var lossPercentage = (loss / intial) * 100;
+    if (initial > current) { // loss logic here 
+        var loss = (initial - current) * quantity;
+        var lossPercentage = (loss / initial) * 100;
 
         showMessage(`Hey! the loss is ${loss} and the percent is ${lossPercentage} %`)
 
 
-    } else if (current > intial) { // profit logic here
-        var profit = (current - intial) * quantity;
-        var profitPercentage = (profit / intial) * 100;
+    } else if (current > initial) { // profit logic here
+        var profit = (current - initial) * quantity;
+        var profitPercentage = (profit / initial) * 100;
 
         showMessage(`Hey the profit is ${profit} and the percent is ${profitPercentage} %`)
 
-    } else { //  rest logic here
-        //showMessage(`Hey !No pain No Gain and No gain No pain !`)
-
     }
-
+    // else { //  rest logic here
+    // showMessage(`Hey !No pain No Gain and No gain No pain !`)
+    //}
 }
 
 function showMessage(message) {
